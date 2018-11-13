4463d3be1a8d76a5e0bdcc82ed569fcf67658959
Spencer White <spencer.white@carat.com>
2018-11-13 11:21:02 -0500

    added createStore and provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import chat from "./reducers";
+import * as types from "../constants/ActionTypes";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create the store, passing it a reducer that will handle dispatched actions
+const store = createStore(chat);
+
+//Add Provider, which will provide access to the store to all nested components
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
