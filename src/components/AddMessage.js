e627934d9e08fdc7646a00128303196dfb25249a
Spencer White <spencer.white@carat.com>
2018-11-13 12:13:55 -0500

    addMessage functional component with proptypes

---
+++ src/components/AddMessage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//Functional component that receives props and just renders an input
+const AddMessage = props => {
+    let input;
+
+    return (
+        <section id="new-message">
+            <input
+                onKeyPress={e => {
+                    if (e.key === "Enter") {
+                        props.dispatch(input.value, "Me");
+                        input.value = "";
+                    }
+                }}
+                type="text"
+                ref={node => {
+                    input = node;
+                }}
+            />
+        </section>
+    );
+};
+
+//Prop types for some basic type checking
+AddMessage.propTypes = {
+    dispatch: PropTypes.func.isReqired
+};
+
+export default AddMessage;
